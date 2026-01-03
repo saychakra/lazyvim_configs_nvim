f66ab7b6c87906dcff8280ba08acabac5b804aa1
Sayan Chakraborty <saychakra@gmail.com>
2026-01-03 11:51:52 +0530

    added keymappings (#1)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+-- for going back to normal mode from insert mode and saving a file in insert mode
+vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = false })
+vim.api.nvim_set_keymap("i", "ss", "<ESC>:write<CR>", { noremap = false })
