b32a4a82c7b748a598a7f287a94bc41cf435bee0
Sayan Chakraborty <saychakra@gmail.com>
2026-01-03 18:07:25 +0530

    quatro setup with qmd files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,15 @@ vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = false })
 vim.api.nvim_set_keymap("i", "ss", "<ESC>:write<CR>", { noremap = false })
 vim.api.nvim_set_keymap("n", "ss", ":write<CR>", { noremap = false })
 vim.api.nvim_set_keymap("n", "sa", "ggVG", { noremap = false })
+
+vim.keymap.set({ "n", "i" }, "<leader>qp", function()
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  vim.api.nvim_buf_set_lines(0, row, row, false, {
+    "```{python}",
+    "",
+    "```",
+  })
+  -- move cursor to the blank line inside the block
+  vim.api.nvim_win_set_cursor(0, { row + 2, 0 })
+  vim.cmd("startinsert")
+end, { desc = "Insert Quarto python block" })
